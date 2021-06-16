936e60ee38ec60786c2623e7049814cf5f21cf4a
edward frempong <eddysoftwarengineer@gmail.com>
2021-06-16 16:31:36 +0000

    install all dependencies for firebase and created config.js file

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+
+
+import firebase from 'firebase';
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCwJ_NSB0qqZDKPLyvGL3-MkvL1y8lKukY",
+    authDomain: "codetrainproject-af048.firebaseapp.com",
+    projectId: "codetrainproject-af048",
+    storageBucket: "codetrainproject-af048.appspot.com",
+    messagingSenderId: "234987614181",
+    appId: "1:234987614181:web:5c244a1242b00e3177890c",
+    measurementId: "G-87G6MH8HPN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  export default firebase;
