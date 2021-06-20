b5caa5f15955428053e66f496025a8b2fb3cf117
edward frempong <eddysoftwarengineer@gmail.com>
2021-06-20 05:19:17 +0000

    changed api-key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import 'firebase/firestore';
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyCwJ_NSB0qqZDKPLyvGL3-MkvL1y8lKukY",
-    authDomain: "codetrainproject-af048.firebaseapp.com",
-    projectId: "codetrainproject-af048",
-    storageBucket: "codetrainproject-af048.appspot.com",
-    messagingSenderId: "234987614181",
-    appId: "1:234987614181:web:5c244a1242b00e3177890c",
-    measurementId: "G-87G6MH8HPN"
+    apiKey: "AIzaSyDQ3Tqw2Dr9m5RGhKp8VKz3N4gCEtCo9XI",
+    authDomain: "expense-c4029.firebaseapp.com",
+    projectId: "expense-c4029",
+    storageBucket: "expense-c4029.appspot.com",
+    messagingSenderId: "217318448959",
+    appId: "1:217318448959:web:259a88076063aa44837d8a",
+    measurementId: "G-X3NG8KEWJF"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
