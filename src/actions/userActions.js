a1e9cefa8fd76b9064e3343f5b40d3d4099d26ab
edward frempong <eddysoftwarengineer@gmail.com>
2021-09-02 14:48:41 +0000

    added data to firestore db but not displayed on front end

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,8 +1,15 @@
-export function addUserAction(user) {
-    return {
-      type: 'ADD_USER',
-      payload: user,
-    };
+export const addUserAction =(user) => {
+    return (dispatch, state, {getFirestore})=>{
+      getFirestore()
+      .collection("users")
+      .add({...user, timestamp: getFirestore().FieldValue.serverTimestamp()})
+      .then((doc)=>{});
+    }
+
+    // {
+    //   type: 'ADD_USER',
+    //   payload: user,
+    // };
   }
   
   export function deleteUserAction(id) {
